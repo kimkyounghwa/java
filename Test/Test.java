a783c6ce010208a1258fe0407bda6f19c49c7173
kimkyounghwa <lrofor7rof@naver.com>
2025-10-18 12:43:01 +0900

    Update Test.java
    ㅎㅎ

--- Test/Test.java
+++ Test/Test.java
@@ -3,6 +3,6 @@ package Test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("깃허브테스중...");
-        System.out.println("10/18 집에서 깃허브 설정중");
+        System.out.println("10/18 집에서 깃허브 설정중,학원에서작업");
     }
 }
